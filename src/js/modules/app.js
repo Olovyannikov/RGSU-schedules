c7c1d4ec552575b5244e226cabbc88ea159ec21f
Илья Оловянников <OlovyannikovIV@rgsu.net>
2021-01-22 09:26:11 +0300

    Разделяет модули в исходниках JS

---
+++ src/js/modules/app.js
@@ -0,0 +1,62 @@
+import Vue from 'vue';
+
+export default () => {
+    const vm = new Vue({
+        el: "#app",
+        data: {
+            keyword: '',
+            test: true,
+            dataList: [],
+            selected: {},
+            highlight: 0
+        },
+        computed: {
+            filteredItems() {
+                return this.dataList
+                    .filter(item => {
+                        return this.keyword !== undefined ? item.city.toLowerCase().includes(this.keyword.toLowerCase()) : '';
+                    })
+                    .slice(0, 3);
+            }
+        },
+        methods: {
+            onList(val) {
+                this.selected = val;
+            },
+            onInput(value) {
+
+                if (value === '') this.selected = {};
+                if (this.filteredItems.length == 1 && this.highlight) this.highlight = 0;
+                //this.test = /^[ а-яА-я]/.test(this.keyword);
+
+            },
+            onSelect(val) {
+                this.selected = val;
+                this.keyword = this.selected.city;
+                //
+                this.highlight = 0;
+            },
+            moveDown() {
+                //this.highlight++
+                if (!this.keyword) return;
+                this.highlight = (this.highlight + 1) % this.filteredItems.length;
+            },
+            moveUp() {
+                if (!this.keyword) return;
+                this.highlight =
+                    this.highlight - 1 < 0
+                        ? this.filteredItems.length - 1
+                        : this.highlight - 1;
+            },
+            mark(word) {
+                const regex = new RegExp("(" + this.keyword + ")", "gi");
+                return word.replace(regex, "<mark>$1</mark>");
+            }
+        },
+        mounted() {
+            fetch("https://gist.githubusercontent.com/gorborukov/0722a93c35dfba96337b/raw/435b297ac6d90d13a68935e1ec7a69a225969e58/russia")
+                .then(r => r.json())
+                .then(data => (this.dataList = data));
+        }
+    });
+}
